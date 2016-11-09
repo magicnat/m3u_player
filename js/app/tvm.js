b0fa309170ab3d4044a4c3de24649876e08b9f3a
NAT <nat@magicnat.com>
2016-11-09 14:35:41 -0500

    done UI, donw setting saving, itemInfo in progress.

--- js/app/tvm.js
+++ js/app/tvm.js
@@ -7,24 +7,29 @@ var tvmPlayer = (function() {
   var fn = {
     
     // 獲取節目列表
-    getChannel : function() {
+    getChannel : function(url) {
       var xmlHttp = new XMLHttpRequest(),
-          channelList = document.getElementById('channelList'),
-          url = fn.getApiUrl('channel') + '';
-      // 讀取遠端 JSON
+          channelList = document.getElementById('channelList');
+      // 讀取遠端 m3u
       xmlHttp.onreadystatechange = function() {
         if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-          var jsonContent = JSON.parse(xmlHttp.responseText);
-          for(key in jsonContent) {
-            var ch = jsonContent[key],
-                item = document.createElement('li');
-            item.title = ch.name;
-            item.setAttribute('data-id', ch.id);
-            item.innerHTML = '<span class="num">' + ch.id + '</span>' + '<span class="t">' + ch.name + '</span>';
+          var m3uContent = m3u.parse(xmlHttp.responseText),
+              i = 0;
+          for(var i = 0; i < m3uContent.length; i++) {
+            var id = ++i,
+                ch = m3uContent[i],
+                title = ch.name,
+                url = ch.url,
+                item = document.createElement('li')
+            item.title = title;
+            item.setAttribute('data-id', id);
+            item.setAttribute('play-url', url);
+            item.innerHTML = '<span class="num">' + id + '</span>' + '<span class="t">' + title + '</span>';
             channelList.appendChild(item);
             item.onclick = function() {
+              var url = this.getAttribute('play-url');
               var id = this.getAttribute('data-id');
-              fn.playChannel(id);
+              fn.playUrl(id, url);
             };
           }
         }
@@ -32,26 +37,33 @@ var tvmPlayer = (function() {
       xmlHttp.open('GET', url, true);
       xmlHttp.send();
     },
-    
-    // 播放指定節目
-    playChannel : function(id) {
-      var xmlHttp = new XMLHttpRequest(),
-          url = fn.getApiUrl('url') + '?user=minalinsky&password=svxcee&id=' + id;
-      // 讀取遠端 JSON
-      xmlHttp.onreadystatechange = function() {
-        if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-          var jsonContent = JSON.parse(xmlHttp.responseText);
-          player.src({
-            src: jsonContent.url,
-            type: 'application/x-mpegURL'
-          });
-          player.play();
-        }
-      }
-      xmlHttp.open('GET', url, true);
-      xmlHttp.send();
+
+    // 播放指定的 m3u8
+    playUrl: function(id, url) {
+      player.src({
+        src: url,
+        type: 'application/x-mpegURL'
+      });
+      player.play();
       fn.setHighlight(id);
     },
+    
+    // 播放指定節目
+    /*playChannel : function(id) {
+      var list = document.getElementById('channelList').childNodes;
+      for(var i = 0; i < list.length; i++) {
+        var ch = list[i];
+        if(ch.getAttribute('data-id') == id) {
+          player.src({
+            src: ch.getAttribute('playUrl'),
+            type: 'application/x-mpegURL'
+	  });
+          break;
+        }
+      }
+      player.play();
+      fn.setHighlight(id);
+    },*/
     
     // 設定正在播放節目
     setHighlight : function(id) {
@@ -106,9 +118,59 @@ var tvmPlayer = (function() {
       }
       action();
     },
+
+    loadSetting : function() {
+      try {
+        var settingObj = JSON.parse(localStorage["playerSettings"]),
+            url = settingObj.url;
+        fn.getChannel(url);
+      } catch (e) {
+        fn.promptSetting();
+      }
+ 
+    },
+
+    // 設置窗口
+    promptSetting : function() {
+      var mask = fn.drawMask('setting'),
+          settingWin = document.createElement('div');
+          settingWin.className = 'setting_panel win';
+          settingWin.innerHTML = '<span class="t">設置</span><span id="win_close" class="dismiss">✕</span><div class="ent"><input type="text"" id="m3uVal" placeholder="m3u 地址"><input type="button" id="doneBtn" value="完成"></div>';
+      mask.appendChild(settingWin);
+      var donebtn = document.getElementById('doneBtn'),
+          closebtn = document.getElementById('win_close');
+      closebtn.onclick = function() {
+        fn.removeWin('setting');
+      };
+      donebtn.onclick = function() {
+        var url = document.getElementById('m3uVal').value,
+            playerSettings = {};
+        playerSettings.url = url;
+        localStorage["playerSettings"] = JSON.stringify(playerSettings);
+        fn.getChannel(url);
+        fn.removeWin('setting');
+      };
+    },
+
+    // 繪製藉着層
+    drawMask : function(id) {
+      var root = document.getElementsByClassName('wrap'),
+          mask = document.createElement('div');
+          mask.id = id;
+      mask.className = 'mask';
+      root[0].appendChild(mask);
+      return mask;
+    },
+
+    // 刪除彈窗
+    removeWin : function(id) {
+      var root = document.getElementsByClassName('wrap'),
+          target = document.getElementById(id);
+      root[0].removeChild(target);
+    }
     
     // 獲取伺服器完整地址
-    getApiUrl : function(apiName) {
+    /*getApiUrl : function(apiName) {
       var url = '';
       if(tvmConf['ssl']) {
         url += 'https://';
@@ -117,13 +179,15 @@ var tvmPlayer = (function() {
       }
       url += tvmConf['host'] + ':' + tvmConf['port'] + '/' + tvmConf['path'] + '/api/' + apiName + '.php';
       return url;
-    }
+    }*/
     
   },
       
   init = function() {
     fn.initUI();
-    fn.getChannel();
+    fn.loadSetting();
+    //fn.promptSetting();
+    //fn.getChannel();
   };
   
   return  {
